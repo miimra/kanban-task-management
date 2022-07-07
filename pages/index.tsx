f37f8cb89a6e35ac86c49163e8da3206b79f8c64
Maryam Rahimi <maryam.rahimi@iodigital.com>
2022-07-07 22:44:57 +0200

    remove Switch component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,12 @@ import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 import Board from "../components/Board";
 import { initialData } from '../data/initialData'
-import Switch from '../components/Switch';
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         Kanban Board
-        {/* <Switch /> */}
       </header>
       <main className={styles.main}>
         <Board data={initialData} />
