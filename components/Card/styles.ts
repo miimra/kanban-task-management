cb2f76614c2eb667b2c560623f36fef806f3c3e3
Maryam Rahimi <maryam.rahimi@iodigital.com>
2022-07-08 00:29:18 +0200

    add dark mode theme

--- components/Card/styles.ts
+++ components/Card/styles.ts
@@ -3,7 +3,6 @@ import styled from '@emotion/styled'
 export const CardContainer = styled('div')`
   display: flex;
   padding: 1rem;
-  background-color: #fafafa;
   margin-bottom: 1rem;
   border: 1px solid #e7e7e7;
   border-radius: 6px;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,56 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { useState } from 'react';
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 import Board from "../components/Board";
 import { initialData } from '../data/initialData'
+import Switch from '../components/Switch';
+import { jsx, ThemeProvider } from '@emotion/react'
+
+export const darkTheme = {
+  color: {
+    primary: '#f7f7f7',
+    darkIndigo: '#071530',
+    paleGray: '#252525',
+    background: '#1e1e1e',
+    text: '#ffffff',
+  },
+};
+
+export const lightTheme = {
+  color: {
+    primary: '#444444',
+    darkIndigo: '#071530',
+    paleGray: '#f2f2f2',
+    background: '#fefefe',
+    text: '#252525',
+  },
+};
 
 const Home: NextPage = () => {
+  const [checked, setChecked] = useState(false);
+
+  function toggleTheme(): void {
+    setChecked(!checked);
+  }
+
   return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        Kanban Board
-      </header>
-      <main className={styles.main}>
-        <Board data={initialData} />
-      </main>
-    </div>
+    <ThemeProvider theme={checked ? darkTheme : lightTheme}>
+      <div className={styles.container}
+        css={(theme: any) => ({
+          color: theme.color.primary,
+          background: theme.color.background
+        })}>
+        <header className={styles.header}>
+          Kanban Board
+          <Switch onChange={toggleTheme} />
+        </header>
+        <main className={styles.main}>
+          <Board data={initialData} />
+        </main>
+      </div>
+    </ThemeProvider>
   )
 }
 
